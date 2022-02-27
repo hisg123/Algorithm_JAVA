3ab294a7a91d68fafe0106311afa5bdde6ed49e1
hisg123 <72256768+hisg123@users.noreply.github.com>
2022-02-27 20:44:08 +0900

    solve 11021
    solve 11021

---
+++ BaekJoon/src/For문/_11021.java
@@ -0,0 +1,26 @@
+package For¹®;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class _11021 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i< T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			bw.write("Case #" + (i+1) + ": " + (A+B) + "\n");
+		}
+		
+		bw.close();
+		br.close();
+		
+	}
+}
