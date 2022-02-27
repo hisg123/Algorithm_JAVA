9fa7715f36734aedf02e1000a17e40705a0b40d6
hisg123 <72256768+hisg123@users.noreply.github.com>
2022-02-27 18:16:24 +0900

    solve 8393
    solve 8393

--- BaekJoon/src/For문/_8393.java
+++ BaekJoon/src/For문/_8393.java
@@ -1,5 +1,19 @@
 package For¹®;
 
-public class _8393 {
+import java.util.Scanner;
 
+public class _8393 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		int sum = 0;
+		
+		for (int i = 1; i < n+1 ; i++) {
+			sum += i;
+		}
+
+		System.out.println(sum);
+		scan.close();
+	}
 }
