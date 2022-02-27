f4f3600a321dd081e92a259a336da9b35a7f58e2
hisg123 <72256768+hisg123@users.noreply.github.com>
2022-02-27 18:04:36 +0900

    Solve 10950
    Solve 10950

---
+++ BaekJoon/src/For문/_10950.java
@@ -0,0 +1,18 @@
+package For¹®;
+
+import java.util.Scanner;
+
+public class _10950 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int T = scan.nextInt();
+		
+		for (int i = 0; i< T; i++) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			System.out.println(A+B);
+		}
+	}
+	
+}
