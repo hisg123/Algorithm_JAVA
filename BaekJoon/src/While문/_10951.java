f97d0c2e0e3590abc3a9a8d8691d013ee6de76c8
hisg123 <72256768+hisg123@users.noreply.github.com>
2022-02-28 23:34:02 +0900

    solve 10951
    solve 10951

---
+++ BaekJoon/src/While문/_10951.java
@@ -0,0 +1,18 @@
+package While¹®;
+
+import java.util.Scanner;
+
+public class _10951 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextInt()) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			System.out.println(A+B);
+		}
+		
+		scan.close();
+	}
+}
