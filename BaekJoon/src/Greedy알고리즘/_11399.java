f52f33df635b9ba739986ac3b9f6c6444ba65959
InKi Hong <72256768+hisg123@users.noreply.github.com>
2022-03-27 22:13:56 +0900

    Solve _11399

---
+++ BaekJoon/src/Greedy알고리즘/_11399.java
@@ -0,0 +1,34 @@
+package Greedy알고리즘;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class _11399 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int P[] = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			P[i] = scan.nextInt();
+		}
+
+		Arrays.sort(P);
+
+		for (int i = 0; i < N; i++) {
+//			System.out.println(P[i]);
+			if (i != 0) {
+				P[i] += P[i - 1];
+			}
+		}
+		
+		int sum = 0;
+		for (int i : P) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+
+	}
+}
